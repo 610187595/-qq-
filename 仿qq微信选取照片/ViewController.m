209351fdee5f3d8ae31c783204064b1db4e6e919
xuezejun <sxnwcxd@163.com>
2015-12-21 17:20:10 +0800

    Initial Commit

---
+++ 仿qq微信选取照片/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  仿qq微信选取照片
+//
+//  Created by 薛泽军 on 15/12/21.
+//  Copyright © 2015年 薛泽军. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
